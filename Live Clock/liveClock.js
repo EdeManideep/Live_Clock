56ed8c773aad9061b0ca932efd80feeb9bbe7ead
EDE MANIDEEP <manideepede9@gmail.com>
2024-08-31 21:42:02 +0530

    Update liveClock.js

--- Live Clock/liveClock.js
+++ Live Clock/liveClock.js
@@ -4,6 +4,7 @@ const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
 const timezoneSelect = document.getElementById('timezoneSelect');
+const switchButton = document.getElementById('switch');
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -35,56 +36,18 @@ function setTime() {
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+};
 
 setTime();
 setInterval(setTime, 1000);
 
+timezoneSelect.addEventListener('change', setTime);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const switchButton = document.getElementById('switch');
-    let numToggle = 0;
-    
-    switchButton.addEventListener('change', () => {
-      document.documentElement.classList.toggle('dark', switchButton.checked);
-
-      if(numToggle === 0){
-        var duration = 500;
-        var animationEnd = Date.now() + duration;
-        var skew = 1;
-
-        function randomInRange(min, max) {
-          return Math.random() * (max - min) + min;
-        }
-
-        (function frame() {
-          var timeLeft = animationEnd - Date.now();
-          var ticks = Math.max(200, 500 * (timeLeft / duration));
-          skew = Math.max(0.8, skew - 0.001);
-
-          confetti({
-            particleCount: 1,
-            startVelocity: 0,
-            ticks: ticks,
-            origin: {
-              x: Math.random(),
-              y: (Math.random() * skew) - 0.2
-            },
-            colors: ['#ffffff'],
-            shapes: ['circle'],
-            gravity: randomInRange(0.4, 0.6),
-            scalar: randomInRange(0.4, 1),
-            drift: randomInRange(-0.4, 0.4)
-          });
-
-          if (timeLeft > 0) {
-            requestAnimationFrame(frame);
-          }
-        }());
-        numToggle = 1;
-      }else{
-        numToggle = 0;
-      }
-      
+    switchButton.addEventListener('change', (e) => {
+        document.documentElement.classList.toggle('dark', e.target.checked);
     });
-  });  
+
+    // Set the toggle state to match the dark mode state on load
+    switchButton.checked = document.documentElement.classList.contains('dark');
+});
